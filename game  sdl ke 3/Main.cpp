a3e02ec8fb0c3d3e6f596ffdd2053dc0ab1518b2
Faiz <Faiz@DESKTOP-R5Q7UIT>
2026-04-26 08:24:25 +0700

    n

--- game  sdl ke 3/Main.cpp
+++ game  sdl ke 3/Main.cpp
@@ -3,63 +3,44 @@
 
 int main(int argc, char* argv[])
 {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL could not initialize! SDL_Error: "
-            << SDL_GetError() << std::endl;
-        return 1;
-    }
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
 
-    // Create Window
     SDL_Window* window = SDL_CreateWindow(
         "SDL2 Test",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         1000, 600,
-        SDL_WINDOW_SHOWN
+        SDL_WINDOW_RESIZABLE // Keep this ON so the Maximize button stays active
     );
 
-    if (window == nullptr)
-    {
-        std::cout << "Window could not be created! SDL_Error: "
-            << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
+    // FIX: Instead of disabling resize, we lock the minimum size.
+    // This prevents the user from shrinking the window.
+    SDL_SetWindowMinimumSize(window, 1000, 600);
 
-    // Create Renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        SDL_RENDERER_ACCELERATED
-    );
-
-    if (renderer == nullptr)
-    {
-        std::cout << "Renderer could not be created! SDL_Error: "
-            << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    // ONLY CREATE THE RENDERER ONCE
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    // Set logical size so the drawing scales when maximized
+    SDL_RenderSetLogicalSize(renderer, 1000, 600);
 
     bool running = true;
     SDL_Event event;
-
     while (running)
     {
         while (SDL_PollEvent(&event))
         {
-            if (event.type == SDL_QUIT)
-                running = false;
+            if (event.type == SDL_QUIT) running = false;
+            
+            // OPTIONAL: If you want to toggle Maximize with a key (like 'M')
+            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_m) {
+                Uint32 flags = SDL_GetWindowFlags(window);
+                if (flags & SDL_WINDOW_MAXIMIZED) SDL_RestoreWindow(window);
+                else SDL_MaximizeWindow(window);
+            }
         }
 
-        // Background color = blue
         SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
         SDL_RenderClear(renderer);
 
-        // Draw red rectangle
         SDL_Rect rect = { 300, 200, 200, 150 };
         SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
         SDL_RenderFillRect(renderer, &rect);
@@ -67,10 +48,8 @@ int main(int argc, char* argv[])
         SDL_RenderPresent(renderer);
     }
 
-    // Cleanup
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
